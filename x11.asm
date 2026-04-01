972815a16b01094d138c322dd2cb7dc95515ca17
thetrung <deulamco@gmail.com>
2026-04-01 23:28:16 +0700

    add child window > x11.asm

--- x11.asm
+++ x11.asm
@@ -6,12 +6,35 @@ section '.data' writeable
 ; ============================================================
 WM_DELETE_WINDOW db "WM_DELETE_WINDOW",0x0
 str_len = $ - WM_DELETE_WINDOW
+
+TITLE_MAIN db "MAIN WINDOW",0x0
+str_len2 = $ - TITLE_MAIN
+
+TITLE_CHILD db "CHILD WINDOW",0x0
+str_len3 = $ - TITLE_CHILD
+
+msg_resize_window db "resize ~> %d x %d",0xA,0
+msg_event         db "event: %d",0xA,0
+
 COLOR_BLACK equ 0x000000
 COLOR_WHITE equ 0xFFFFFF
+COLOR_RED   equ 0xFFF000
+
+PARENT_WIDTH  equ 800
+PARENT_HEIGHT equ 600
+CHILD_WIDTH   equ 400
+CHILD_HEIGHT  equ 400
+OFFSET_TITLE  equ 100
+TOP_THICK     equ 50
+
 
 ; Reserve space for X11 structs and pointers
 display_ptr dq 0
 window      dq 0
+w_width     dq 0
+w_height    dq 0
+root        dq 0
+child       dq 0
 screen      dd 0
 gc          dq 0
 event       rb 64            ; XEvent is big, but we only need 64 bytes
@@ -84,13 +107,20 @@ _start:
 
     ; Root window
     invoke XRootWindow, [display_ptr], qword [screen]
-    mov [window], rax         ; temporarily store root here
+    mov [root], rax         ; temporarily store root here
     
     ; Create simple window 
     ; XCreateSimpleWindow(Display*, RootWindow, x, y, width, height, border, foreground, background)
-    invoke XCreateSimpleWindow, [display_ptr], [window], 100, 100, 400, 300, 0, COLOR_WHITE, COLOR_BLACK
-    mov [window], rax         ; store window ID
+    invoke XCreateSimpleWindow, [display_ptr], [root], \
+           0, 0, PARENT_WIDTH, PARENT_HEIGHT, 0,       \
+           COLOR_WHITE, COLOR_BLACK
+    mov [window], rax ; store window ID
     
+    invoke XCreateSimpleWindow, [display_ptr], [window],\
+           50, 400, CHILD_WIDTH, CHILD_HEIGHT, 10,      \
+           COLOR_RED, COLOR_BLACK
+    mov [child], rax ; store window ID -> child 
+
     ; Select key + close events
     mov rdx, (1 shl 17) or (1 shl 15)   ; KeyPress + StructureNotify
     invoke XSelectInput, [display_ptr], rax
@@ -104,6 +134,7 @@ _start:
 
     ; Map the window (show it)
     invoke XMapWindow, [display_ptr], [window]
+    invoke XMapWindow, [display_ptr], [child]
 
     ; Create graphics context
     xor rdx, rdx
@@ -121,19 +152,32 @@ event_loop:
     mov rdi, [display_ptr]
     lea esi, [event]
     call XNextEvent
+    invoke printf, msg_event, qword [event]
 
-    cmp dword [event], 12      ; Wait for Expose Event....
-    jne event_loop             ; Else it show nothing.
+    cmp dword [event], 12
+    jne  after_resize
+    ; cmp dword [event], 12      ; Wait for Expose Event....
+    ; jne event_loop             ; Else it show nothing.
+; Resize Window :
+; This is where I don't want to cont. with X11.
+; And I should make my own DE for FASM.
+    xor rax, rax
+    mov rax, qword [event+48] ; width
+    mov rax, qword [event+52] ; height
+    mov [w_width],  r8
+    mov [w_height], r9
+    invoke printf, msg_resize_window, r8, r9
 ; ===============================
 ; Draw shapes
 ; ===============================
-
+after_resize:
     ; Line from (50,50) -> (350,50)
-    invoke XDrawLine, [display_ptr], [window], [gc], 50, 50, 300, 50
-    invoke XDrawLine, [display_ptr], [window], [gc], 50, 350, 300, 350
+    invoke XDrawLine, [display_ptr], [window], [gc], 0, TOP_THICK, [w_width], TOP_THICK
+    invoke XDrawLine, [display_ptr], [child],  [gc], 0, TOP_THICK, CHILD_WIDTH,  TOP_THICK
 
     ; Text at (128,40) + string_ptr, string_length 
-    invoke XDrawString, [display_ptr], [window], [gc], 120, 40, WM_DELETE_WINDOW, str_len
+    invoke XDrawString, [display_ptr], [window], [gc], OFFSET_TITLE, TOP_THICK/2, TITLE_MAIN, str_len2
+    invoke XDrawString, [display_ptr], [child],  [gc], OFFSET_TITLE, TOP_THICK/2, TITLE_CHILD, str_len3
 
     ; Rectangle outline at (50,70) size 100x50
     invoke XDrawRectangle, [display_ptr], [window], [gc], 50, 70, 100, 50
