6aed4b915add2e624d9f0a07f95b042b5f27332c
thetrung <deulamco@gmail.com>
2026-04-08 00:42:29 +0700

    move concat_string(rdi, rsi, rdx) -> linux64a.inc

--- concat_string.asm
+++ concat_string.asm
@@ -3,34 +3,12 @@ segment readable executable
 entry _start
 include 'linux64a.inc'
 _start:
-	;Copy msg -> buffer :
-	mov esi, msg
+	;Copy address:
 	mov edi, buffer
-
-copy_loop:
-	; copy esi -> edi
-	lodsb
-	stosb
-	test al, al
-	jnz copy_loop
-
-	; if appending text:
-	test r12,r12
-	jnz print_str
-
-	;Copy new_text -> buffer :
-	mov esi, new_text
-	mov r12, 1			; indicate append
-	jmp copy_loop
-
-print_str:
-	mov rax, SYS_WRITE	; sys_write = 1
-	mov rdi, STDOUT ; file description = 1 
-	mov rsi, buffer 	  ; 256 bytes buffer
-	mov rdx, 13			    ; total text length
-	syscall
-
-  sleep secs
+	mov esi, msg.hello
+  mov edx, msg.world
+  call concat_string
+  call print_string
 
 exit:
 	mov rax, SYS_EXIT	; sys_exit = 60
@@ -38,7 +16,7 @@ exit:
 	syscall
 
 segment readable writeable
-secs: dq 10,0 ; timespecs 64-bit
 buffer: rb 256
-msg: db "Hello", 0
-new_text: db " World!", 0
+msg: 
+.hello db "Hello", 0
+.world db " World!", 0
